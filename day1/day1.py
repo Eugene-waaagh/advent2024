36c51a1962eb8c8590efda2bde46a5b1fffddd96
Eugene <qihao.eugenewong@gmail.com>
2024-12-03 17:04:44 +0800

    day1 finished

--- day1/day1.py
+++ day1/day1.py
@@ -19,6 +19,7 @@ for i, num in enumerate(input):
 sorted_left = sorted(left)
 sorted_right = sorted(right)
 
+# Part 1
 total = 0
 for num_left, num_right in zip(sorted_left, sorted_right):
     if num_left >= num_right:
@@ -26,6 +27,17 @@ for num_left, num_right in zip(sorted_left, sorted_right):
     else:
         total = total + (num_right - num_left)
 
+print("Day 1 result:",total)
+
+# Part 2
+total = 0
+dict = {}
+
+for num_left in sorted_left:
+    if num_left in sorted_right:
+        dict[num_left] = dict.get(num_left, 0) + 1
+
+for key, value in dict.items():
+    total += (key * value)
+
 print(total)
-
-
